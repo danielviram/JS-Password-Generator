44988475e47f645da86ea11d082b13ecec854324
Alex Taylor <altays633@gmail.com>
2020-02-04 11:52:17 -0500

    adding validation for confirm character prompts and for number input

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,9 @@ let uppercaseChar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","
 let numericChar=["1","2","3","4","5","6","7","8","9","0"];
 let specialChar=["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/"];
 
+// for (let i = 0; i < Math.floor(specialChar.length); i++) {
+//     console.log(specialChar[i]);
+// }
 //// test the special characters to see what needs escape characters using a loop
 
 // WHEN prompted for password criteria
@@ -22,21 +25,32 @@ let specialChar=["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";"
 // THEN my input should be validated and at least one character type should be selected
 
 //// create an if statement - if user does not select at least one type of character, then they have to start over
+//// use a while loop to check if the use variables are undefined - if so, run the loop
+//// use an if statement to see if they are all false
 
-let useLowercase=confirm("Do you want to include lowercase characters?");
-let useUppercase=confirm("Do you want to include uppercase characters?");
+// declaring boolean variables, asking user for input
+let useLowerCase=confirm("Do you want to include lowercase characters?");
+let useUpperCase=confirm("Do you want to include uppercase characters?");
 let useNumeric=confirm("Do you want to include numeric characters?");
 let useSpecial=confirm("Do you want to include special characters?");
 
+//running loop to check if all variables are false. If so, user is prompted to select again
+while (useLowerCase == false && useUpperCase == false && useNumeric == false && useSpecial == false) {
+    alert("Please select one type of character");
+    let useLowerCase=confirm("Do you want to include lowercase characters?");
+    let useUpperCase=confirm("Do you want to include uppercase characters?");
+    let useNumeric=confirm("Do you want to include numeric characters?");
+    let useSpecial=confirm("Do you want to include special characters?");
+}
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
 
-//// check to make sure that only numbers are entered
-//// check to make sure the number is greater than or equal to 8, less than or equal to 128
+
+// while loop to check for password length. If number less than 8 or over 128, or if input is not a number, user is asked for input again
 
 let passwordLength=prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters.");
-while(passwordLength<8 || passwordLength >128) {
-    alert("Please choose a length that is more than 8 and less than 128 characters");
+while(passwordLength<8 || passwordLength >128 || typeof(passwordLength)!="number") {
+    alert("Please choose a number that is more than 8 and less than 128 characters");
     passwordLength=prompt("How many characters do you want the password to be? The password cannot be less than 8 or more than 128 characters.");
 } 
 
